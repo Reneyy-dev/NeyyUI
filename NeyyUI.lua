e75c27514f01ccefd43700214da92158834422a6
bacotanda925-sketch <bacotanda925@gmail.com>
2026-09-05 05:05:12 +0700

    Add NeyyUI module with CreateWindow function

---
+++ NeyyUI.lua
@@ -0,0 +1,14 @@
+local NeyyUI = {}
+
+function NeyyUI:CreateWindow(config)
+    config = config or {}
+
+    print("[NeyyUI] CreateWindow berhasil")
+    print("[NeyyUI] Title:", config.Title or "Untitled")
+
+    local Window = {}
+
+    return Window
+end
+
+return NeyyUI
